54e03718fb1fdfcb8b7be9eb650875d42e73fd89
nageswararaovutla <nageswararaovutla8466@gmail.com>
2024-08-13 15:56:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
-st.title('🎈 Streamlit App')
+st.title('Welcome to this world')
 
 st.write('Hello world!')
+st.subheader('Enter your country')
+a=st.text_input('enter your name')
+st.write("your name is " a)
